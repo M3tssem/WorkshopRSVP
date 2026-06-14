b3bac6579a482585fee5f32ed2997901d338a54f
Mtassem Saleh <Sale0196@algonquinlive.com>
2026-06-14 14:56:51 -0400

    Workshops landing page and navigation menu

--- Models/Rsvp.cs
+++ Models/Rsvp.cs
@@ -1,9 +1,16 @@
-﻿namespace WorkshopRSVP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkshopRSVP.Models
 {
     public class Rsvp
     {
+        [Display(Name = "Full Name")]
         public string FullName { get; set; } = "";
+
+        [Display(Name = "Needs Accommodation")]
         public bool NeedsAccommodation { get; set; }
+
+        [Display(Name = "Workshop Title")]
         public string WorkshopTitle { get; set; } = "";
     }
 }
