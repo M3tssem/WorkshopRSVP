9c5a87da36c4da9a3363e60a160768002ee68265
Mtassem Saleh <Sale0196@algonquinlive.com>
2026-06-13 22:30:56 -0400

    Added RSVP model and workshops controller actions

---
+++ Controllers/WorkshopsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkshopRSVP.Models;
+
+namespace WorkshopRSVP.Controllers
+{
+    public class WorkshopsController : Controller
+    {
+        private static List<Rsvp> registrations = new List<Rsvp>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Confirm(Rsvp model)
+        {
+            registrations.Add(model);
+            ViewData["Message"] = $"Thanks for registering, {model.FullName}!";
+            return View(model);
+        }
+
+        public IActionResult Registrations()
+        {
+            return View(registrations);
+        }
+    }
+}
---
+++ Models/Rsvp.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkshopRSVP.Models
+{
+    public class Rsvp
+    {
+        public string FullName { get; set; } = "";
+        public bool NeedsAccommodation { get; set; }
+        public string WorkshopTitle { get; set; } = "";
+    }
+}
